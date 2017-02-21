cf08c070562d5dfd44b61fb6e6e5e5376ee24b98
Lee Saville <l.w.saville@gmail.com>
2017-02-21 07:56:25 -0700

    Add simple runner file to test queue informally

--- test.rb
+++
@@ -1,6 +0,0 @@
-require 'faraday'
-
-data = {company: {name: 'Blah'}, token: 'TurMonLook4'}
-blah = Faraday.post('http://turingmonocle-staging.herokuapp.com/api/v1/companies', data)
-
-puts blah.body
